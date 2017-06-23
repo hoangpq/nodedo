92a6cb53adefc51d99df9529f16e432ce3d07fec
Hoang Phan <hoang.phan@sentifi.com>
2017-06-23 15:54:22 +0700

    docs: add some elasticsearch content

--- react-hot/data/reducers.note.js
+++ react-hot/data/reducers.note.js
@@ -28,4 +28,23 @@ Object.freeze(orders);
 
 orders.milk.price -= 15;
 console.log(orders.milk.price);
+
+// Elasticsearch
+
+Partitioning your document into different containers or `shards`
+which can be stored on a single node or on multiple nodes
+
+Balancing these shards across the nodes in your cluster to spread
+the indexing and search load
+
+Duplicating each shard to provide redundant copies of your data,
+to prevent data loss in case of hardware failure
+
+Seamlessly integrating new nodes as your cluster grows or redistributing
+shards to recover from node loss
+
+While Elasticsearch can benefit from more-powerful hardware, vertical
+scale has its limits. Real scalability comes from horizontal scale - the ability
+to add more nodes to the cluster and to spread load and reliability between them.
+
  */
